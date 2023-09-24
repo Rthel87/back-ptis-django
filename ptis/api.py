2aaffaf51d3115b84b77f3850cfa1e1409f488dd
Rodrigo Castillo C <rodrigo.castillo.c@usach.cl>
2023-09-24 17:52:41 -0300

    API: Se agregan rutas para 'estudiante' según configuración base de 'ViewSet'

--- ptis/api.py
+++ ptis/api.py
@@ -1,8 +1,13 @@
 from rest_framework import viewsets, permissions
-from .models import Rol
-from .serializers import RolSerializer
+from .models import Rol, Student
+from .serializers import RolSerializer, StudentSerializer
 
 class RolViewSet(viewsets.ModelViewSet):
     queryset = Rol.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = RolSerializer
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = StudentSerializer
--- ptis/urls.py
+++ ptis/urls.py
@@ -1,8 +1,9 @@
 from rest_framework import routers
-from .api import RolViewSet
+from .api import RolViewSet, StudentViewSet
 
 router = routers.DefaultRouter()
 
 router.register('roles', RolViewSet, 'roles')
+router.register('estudiantes', StudentViewSet, 'estudiantes')
 
 urlpatterns = router.urls
