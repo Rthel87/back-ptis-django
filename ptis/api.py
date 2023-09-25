347eb3f2b6b8b5587f0c825288cd29b24511ae94
Rodrigo Castillo C <rodrigo.castillo.c@usach.cl>
2023-09-24 22:01:16 -0300

    API: Se agregan 'ViewSet' y rutas para el modelo 'Usuario'

--- ptis/api.py
+++ ptis/api.py
@@ -1,11 +1,16 @@
 from rest_framework import viewsets, permissions
-from .models import Rol, Student
-from .serializers import RolSerializer, StudentSerializer
+from .models import Rol, User, Student
+from .serializers import RolSerializer, UserSerializer, StudentSerializer
 
 class RolViewSet(viewsets.ModelViewSet):
     queryset = Rol.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = RolSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = UserSerializer
 
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
--- ptis/urls.py
+++ ptis/urls.py
@@ -1,9 +1,10 @@
 from rest_framework import routers
-from .api import RolViewSet, StudentViewSet
+from .api import RolViewSet, UserViewSet, StudentViewSet
 
 router = routers.DefaultRouter()
 
 router.register('roles', RolViewSet, 'roles')
 router.register('estudiantes', StudentViewSet, 'estudiantes')
+router.register('usuarios', UserViewSet, 'usuarios')
 
 urlpatterns = router.urls
